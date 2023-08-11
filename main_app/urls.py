3b9575e34fb5b3b31aad7e344e656e469bf4baf8
Arjun P <arjunprakash027@gmail.com>
2023-08-11 16:00:04 +0530

    question parsing

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Greeting
+from .views import Greeting,GetUserAnswers
 
 urlpatterns = [
     path('greeting/',Greeting.as_view(),name='greeting'),
+    path('GetRating/',GetUserAnswers.as_view(),name='GetRating'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,30 @@ from rest_framework.response import Response
 
 class Greeting(APIView):
 
-    def get(self, request):
+    def get(self):
+        return Response({"message":"end point working fine"})
+    
+class GetUserAnswers(APIView):
+
+    def post(self,request):
+        subject = request.data.get("UserAnswer")
+        print(type(subject))
+        chatgptsend = "theese are the question i need answer for \n subject:DBMS "
+
+        print(subject[0])
+
+        for x in subject[0]:
+            topic = "\n topic: " + x + "\n"
+            chatgptsend += topic
+            print(x)
+
+        for y in subject[0][x]:
+            for questions in y:
+                question = "question: " + questions + "\n"
+                chatgptsend += question
+                print(questions)
+
+        print(chatgptsend)
         return Response({"message":"end point working fine"})
 
 
