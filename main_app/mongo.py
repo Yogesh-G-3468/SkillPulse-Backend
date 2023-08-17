98e3565289a2ac5893c1b6f787c292b0fbc16b54
Yogesh <63808297+Dedmeat@users.noreply.github.com>
2023-08-17 20:13:23 +0530

    Mongo retrive marks

--- main_app/mongo.py
+++ main_app/mongo.py
@@ -1,55 +1,12 @@
 from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
-# client = MongoClient('mongodb://localhost:27017')
-# db = client['Scoredata']
-# collec=db['testhistory']
-# userid="yogi@mail.com"
-# history = { 
-#    "user_id":"sample@mail.com",
-#    "scores":{ "m1" : {
-#         "c/c++" : {
-#             "subjectName" : "C/C++",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         },
-#         "java" : {
-#             "subjectName" : "JAVA",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         },
-#         "oops": {
-#             "subjectName" : "OOPS",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         },
-#         "dsa" : {
-#             "subjectName" : "DSA",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         }
-#   },
-#   "m2" : {
-#         "dbms" : {
-#             "subjectName" : "DBMS",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         },
-#         "cn" : {
-#             "subjectName" : "CN",
-#             "entryTest" : False,
-#             "exitTest" :False
-#         },
-#         "os" : {
-#             "subjectName" : "OS",
-#             "entryTest" : False,
-#             "exitTest" : False
-#         }}
-#   }}
 
+jumbla = "enter paswrod"
 
 
 def MongoInsertTest(history):
-    client = MongoClient('mongodb://localhost:27017')
+    client = MongoClient('mongodb+srv://test:{}@cluster0.1y89bs5.mongodb.net/?retryWrites=true&w=majority'.format(jumbla),server_api=ServerApi('1'))
     db = client['Scoredata']
     collec=db['testhistory']
     id = collec.insert_one(history)
@@ -57,8 +14,29 @@ def MongoInsertTest(history):
 
 
 def MongoInsertTotalMark(totmark):
-    client = MongoClient('mongodb://localhost:27017')
+    client = MongoClient('mongodb+srv://test:{}@cluster0.1y89bs5.mongodb.net/?retryWrites=true&w=majority'.format(jumbla),server_api=ServerApi('1'))
     db = client['Scoredata']
     collection = db['totalmark']
     id=collection.insert_one(totmark)
     print(id.inserted_id)
+
+
+
+
+def MongoRetirveTest(userid):
+    client = MongoClient('mongodb+srv://test:{}@cluster0.1y89bs5.mongodb.net/?retryWrites=true&w=majority'.format(jumbla),server_api=ServerApi('1'))
+    db = client['Scoredata']
+    collection = db['testhistory']
+    for x in collection.find({'user_id':userid}):
+        return(x)
+
+def MongoRetirveTotalMarks(userid):
+    client = MongoClient('mongodb+srv://test:{}@cluster0.1y89bs5.mongodb.net/?retryWrites=true&w=majority'.format(jumbla),server_api=ServerApi('1'))
+    db = client['Scoredata']
+    collection = db['totalmark']
+    for x in collection.find({'user_id':userid}):
+        return(x)
+
+
+
+
