54a33e9a59484d72322158f98a9a4e350aa6395e
Arjun P <arjunprakash027@gmail.com>
2023-08-20 10:58:30 +0530

    ADDED TESTS

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,34 +1,16 @@
 from django.test import TestCase
-import unittest
-import django
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-from django.contrib.auth.models import User
-from .views import RegisterNewUser, Greeting, TestHistory, TestMark, RatingRetrive, GetUserAnswers
-
-
-class TestRegisterNewUser(unittest.TestCase):
+class TestRegisterNewUser(TestCase):
 
     def test_create_new_user(self):
         email = "test@gmail.com"
         password = "password"
 
-        user = RegisterNewUser.as_view()
-        response = user(request=django.http.HttpRequest, data={"email": email, "password": password})
+        client = APIClient()
+
+        response = client.post(reverse('register-user'), {'email': email, 'password': password})
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["message"], "User {} Created Successfully".format(email))
-
-
-class TestGreeting(unittest.TestCase):
-
-    def test_get_greeting_message(self):
-        user = User.objects.create_user(username="test", password="password")
-
-        greeting = Greeting.as_view()
-        response = greeting(request=None, headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Token d2b40cba71908e81b165342b182ee723e00cbaf7"
-  })
-
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["message"], "HI yogi@gmail.com")
+        self.assertEqual(response.data["message"], f"User {email} Created Successfully")
